6bdbd18414eaa0f0b95a2bdc00cafdd9f1c2402f
Crypted <shedrack_samaila@yahoo.com>
2025-07-02 14:26:54 +0100

    depoly fix

--- build/server.js
+++ build/server.js
@@ -12,13 +12,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fastify_1 = require("fastify");
+const fastify_1 = __importDefault(require("fastify"));
 const index_route_1 = require("./routes/index.route");
 const mongoose_1 = __importDefault(require("mongoose"));
 const auth_middleware_1 = __importDefault(require("./middleware/auth.middleware"));
 require("dotenv").config();
+const autoload_1 = __importDefault(require("@fastify/autoload"));
+const path_1 = require("path");
 const PORT = process.env.PORT || 3000;
-const server = (0, fastify_1.fastify)({
+const app = (0, fastify_1.default)({
     logger: true,
 });
 mongoose_1.default.connect(process.env.MONGO_URI);
@@ -28,11 +30,22 @@ mongoose_1.default.connection.on("error", (err) => {
 mongoose_1.default.connection.on("connected", () => {
     console.log("MongoDB connected successfully");
 });
-server.register(index_route_1.indexRoute, { prefix: "/api/v1" });
-server.addHook("preHandler", auth_middleware_1.default);
+const pluginOptions = {
+// Place your custom options the autoload plugin below here.
+};
+// app.register(AutoLoad, {
+//   dir: join(__dirname, 'plugins'),
+//   options: pluginOptions
+// });
+app.register(autoload_1.default, {
+    dir: (0, path_1.join)(__dirname, 'routes'),
+    options: pluginOptions
+});
+app.register(index_route_1.indexRoute, { prefix: "/api/v1" });
+app.addHook("preHandler", auth_middleware_1.default);
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield server.listen({ port: PORT });
+        yield app.listen({ port: PORT });
     }
     catch (err) {
         console.error(err);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import { fastify, FastifyInstance } from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import {indexRoute} from "./routes/index.route";
 import mongoose from "mongoose";
 import authenticate from "./middleware/auth.middleware";
 require("dotenv").config();
+import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
+import { join } from 'path';
+
 const PORT = process.env.PORT || 3000;
 
-const server: FastifyInstance = fastify({
+const app: FastifyInstance = fastify({
   logger: true,
 });
 
@@ -17,12 +20,27 @@ mongoose.connection.on("error", (err) => {
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected successfully");
 });
-server.register(indexRoute,  {prefix: "/api/v1"});
-server.addHook("preHandler", authenticate);
+
+const pluginOptions: Partial<AutoloadPluginOptions> = {
+  // Place your custom options the autoload plugin below here.
+}
+
+// app.register(AutoLoad, {
+//   dir: join(__dirname, 'plugins'),
+//   options: pluginOptions
+// });
+
+app.register(AutoLoad, {
+  dir: join(__dirname, 'routes'),
+  options: pluginOptions
+});
+
+app.register(indexRoute,  {prefix: "/api/v1"});
+app.addHook("preHandler", authenticate);
 
 const start = async () => {
   try {
-    await server.listen({port: PORT as number});
+    await app.listen({port: PORT as number});
   } catch (err) {
     console.error(err);
     process.exit(1);
